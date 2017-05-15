19c47eaa3f4a596e5a05645c497a62f2015edb09
Alessa Messineo <apmessineo@gmail.com>
2017-05-15 16:59:20 -0400

    add cell click events

--- index.js
+++ index.js
@@ -12,3 +12,19 @@ function fillSquare(move) {
     return move.filled;
 }
 document.getElementById('1').innerHTML = fillSquare({ filled: "0" });
+var turnCounter = 0;
+var cells = document.getElementsByClassName('cell');
+console.log('cells: ', cells);
+var _loop_1 = function (i) {
+    cells[i].addEventListener('click', function () {
+        if (turnCounter % 2 === 0) {
+            cells[i].innerHTML = fillSquare({ filled: "X" });
+        }
+        else {
+            cells[i].innerHTML = fillSquare({ filled: "O" });
+        }
+    });
+};
+for (var i = 0; i < cells.length; i++) {
+    _loop_1(i);
+}
--- index.ts
+++ index.ts
@@ -23,8 +23,21 @@ function fillSquare(move: FilledSquare){
   return move.filled;
 }
 
-
-
 document.getElementById('1').innerHTML = fillSquare({filled: "0"});
+
+let turnCounter: number = 0;
+
+let cells: any = document.getElementsByClassName('cell');
+ console.log('cells: ', cells);
+
+ for (let i=0; i<cells.length; i++){
+    cells[i].addEventListener('click', ()=> {
+      if (turnCounter%2 === 0){
+        cells[i].innerHTML = fillSquare({filled: "X"});
+      } else {
+        cells[i].innerHTML = fillSquare({filled: "O"});
+      }
+    })
+ }
 
 
